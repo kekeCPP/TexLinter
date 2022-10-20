9b5be23d1e41b57a88aec2aeef1df8265d33761b
Hampus Grimskär <hampus.grimskar@gmail.com>
2022-10-20 14:16:34 +0200

    main function updated to only accept .tex files

--- zebralint.py
+++ zebralint.py
@@ -14,24 +14,43 @@ def main():
 
     args = parser.parse_args()
 
-    if args.newfile:
-        if os.path.exists(args.file_path):
-            with open(args.file_path, "r", encoding = "utf-8") as file:
-                text = file.read()
-                file.close()
+    file_type = args.file_path[(len(args.file_path) - 4):(len(args.file_path))]
 
-            if not os.path.exists(args.newfile):
-                with open(args.newfile, "w", encoding = "utf-8") as new_file:
-                    text = linter.format_text(text)
-                    new_file.write(text)
-                    new_file.close()
-            else: print("Error: A file with that name already exists!")
+    if args.newfile:
+        newfile_type = args.newfile[(len(args.newfile) - 4):(len(args.newfile))]
+        if os.path.exists(args.file_path):
+            if file_type != ".tex":
+                print("Error: The file you are trying to format is of the wrong type!")
+            else:
+                with open(args.file_path, "r", encoding = "utf-8") as file:
+                    text = file.read()
+                    file.close()
+
+                if not os.path.exists(args.newfile):
+                    if newfile_type != ".tex":
+                        print("Error: The new file needs to be of .tex type!")
+                    else:
+                        with open(args.newfile, "w", encoding = "utf-8") as new_file:
+                            text = linter.format_text(text)
+                            new_file.write(text)
+                            new_file.close()
+                else: print("Error: A file with that name already exists!")
+        else:
+            print("Error: The file you are trying to format doesn't exist!")
     else:
         if os.path.exists(args.file_path):
-            with open(args.file_path, "w", encoding = "utf-8") as file:
-                text = linter.format_text(text)
-                file.write(text)
-                file.close()
+            if file_type != ".tex":
+                print("Error: The file you are trying to format is of the wrong type!")
+            else:
+                with open(args.file_path, "r", encoding = "utf-8") as file:
+                    text = file.read()
+                    text = linter.format_text(text)
+                    file.close()
+                with open(args.file_path, "w", encoding = "utf-8") as file:
+                    file.write(text)
+                    file.close()
+        else:
+            print("Error: The file you are trying to format doesn't exist!")
 
 if __name__ == "__main__":
     main()
